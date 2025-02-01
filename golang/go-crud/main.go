89a112112a699dd9da7abd933a89c133d2f9278f
Isaac Hermel <isaachermel@gmail.com>
2025-01-31 23:22:04 -0300

    golang: new language to the project

---
+++ golang/go-crud/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Contact struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+type ContactService struct {
+	Contacts map[int]Contact
+}
+
+func (c *ContactService) Create(w http.ResponseWriter, r *http.Request) {
+	var contact Contact
+	err := json.NewDecoder(r.Body).Decode(&contact)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := len(c.Contacts) + 1
+	contact.Id = id
+
+	c.Contacts[id] = contact
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(contact)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (c *ContactService) List(w http.ResponseWriter, t *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var contacts []Contact
+
+	for _, ct := range c.Contacts {
+		contacts = append(contacts, ct)
+	}
+
+	json.NewEncoder(w).Encode(contacts)
+}
+
+func handleGetContacts(w http.ResponseWriter, t *http.Request, service *ContactService) {
+
+}
+
+func main() {
+	mux := http.NewServeMux()
+	service := &ContactService{Contacts: make(map[int]Contact)}
+
+	mux.HandleFunc("/contacts", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			service.List(w, r)
+		case http.MethodPost:
+			service.Create(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
